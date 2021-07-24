ce4ac1aa3d159635a5b071616c9e16926cf48f25
Maciej 'Matt' Madejsza <maciej.madejsza@gmail.com>
2021-07-24 10:47:46 +0100

    /Counter inserted

--- react-basics-review/src/App.js
+++ react-basics-review/src/App.js
@@ -1,17 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
-import './Counter';
+import Counter from "./Counter"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-
+          JS Counter:
         </p>
+        <Counter myValue="103" />
 
       </header>
-    </div>
+    </div >
   );
 }
 
--- react-basics-review/src/Counter.js
+++ react-basics-review/src/Counter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class Counter extends Component {
     render() {
-        return (<div className="counter">Counter</div>)
+        return (<div className="counter">Counter: {this.props.myValue}</div>)
     }
 }
 
